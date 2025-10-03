5fd121cdfad8976a98070671dec1b51cfb364d7c
warrier090-create <warrier090-create>
2025-10-03 16:14:48 +0000

    bundle development

---
+++ DAB_PROJECT/src/Pipelines/Demopipeline/transformations/transformation.py
@@ -0,0 +1,4 @@
+import dlt
+@dlt.table
+def transformed():
+    return spark.range(10)
---
+++ DAB_PROJECT/tests/main_test.py
@@ -0,0 +1,6 @@
+from DAB_PROJECT import main
+
+
+def test_find_all_taxis():
+    taxis = main.find_all_taxis()
+    assert taxis.count() > 5
